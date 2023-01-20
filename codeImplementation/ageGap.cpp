451da465d961219609e05afa458c35892845ceeb
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-01-20 15:31:26 +0900

    Create ageGap.cpp
    FEAT : gap of Age
    
    User enter the maximum count of values, and write down numbers randomly.
    Program find the maximum and minumum number, and then it prints the gap of the numbers.
    
    See also : I took a class 'it 취업을 위한 알고리즘 문제풀이 입문 (with C/C++) : 코딩테스트 대비'

---
+++ codeImplementation/ageGap.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, a, max = 1, min = 100;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        cin >> a;
+        if (a > max) max = a;
+        if (a < min) min = a;
+    }
+    cout << max - min;
+
+    return 0;
+}
