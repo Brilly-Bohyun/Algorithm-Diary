b072b7de8c979e4492ed27abd12b7cf893430ba8
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-01-20 15:02:46 +0900

    Create sumOfRange.cpp
    FEAT : sum of range
    
    Users enter a range of number.
    Then, program print the formula and calculate the sum of range
    
    See also : I took a class 'it 취업을 위한 알고리즘 문제풀이 입문 (with C/C++) : 코딩테스트 대비'

---
+++ codeImplementation/sumOfRange.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b, i, sum = 0;
+
+	cin >> a >> b;
+
+	for (int i = a; i < b; i++) {
+		sum += i;
+		cout << i << " + ";
+	}
+	cout << b << " = " << sum+b;
+
+	return 0;
+}
