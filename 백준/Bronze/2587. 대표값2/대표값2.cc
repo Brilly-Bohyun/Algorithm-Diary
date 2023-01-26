40f8a8fd4b29796f76ecb505cf111f257223cdd2
Brilly-Bohyun <102973953+Brilly-Bohyun@users.noreply.github.com>
2023-01-26 23:45:06 +0900

    [Bronze II] Title: 대표값2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2587. 대표값2/대표값2.cc
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main(){
+    int arr[5] = {};
+    int total = 0;
+    
+    for(int i = 0; i < 5; i++){
+        cin >> arr[i];
+        total += arr[i];
+    }
+    
+    sort(arr, arr+5);
+    
+    cout << total/5 << "\n" << arr[2] << "\n";
+}
