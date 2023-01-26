328090133f12dbdc1d8a9ab2d4feb4c1492834b8
Brilly-Bohyun <102973953+Brilly-Bohyun@users.noreply.github.com>
2023-01-26 23:20:51 +0900

    [Bronze III] Title: 최댓값, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.cc
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int max=0, maxindex = 0;
+    int arr[9];
+    
+    for(int i=0; i < 9; i++){
+         cin >> arr[i];
+        
+        if(max < arr[i]){
+            max = arr[i];
+            maxindex = i;
+        }
+    }
+    cout << max << "\n" << maxindex+1;
+}
