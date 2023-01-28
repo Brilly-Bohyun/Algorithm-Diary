d8719077c885e7ab50624d3b33d4a4c059215b49
Brilly-Bohyun <102973953+Brilly-Bohyun@users.noreply.github.com>
2023-01-28 23:39:39 +0900

    [Bronze III] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.cc
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a, b, total = 0;
+    cin >> a >> b;
+    
+    cout << a * (b % 10) << "\n";
+    cout << a * (b % 100 / 10) << "\n";
+    cout << a * (b / 100) << "\n";
+    cout << a * b << "\n";
+    return 0;
+    
+}
