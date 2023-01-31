4cc5993dbf995716cbba0452a0790758d0533485
Brilly-Bohyun <102973953+Brilly-Bohyun@users.noreply.github.com>
2023-02-01 01:18:33 +0900

    [Bronze II] Title: 커트라인, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.cc
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main(){
+    int a, b;
+    cin >> a >> b;
+    int arr[a] = {};
+    
+    for(int i=0; i < a; i++){
+        cin >> arr[i];
+    }
+    sort(arr, arr+a, greater<>());
+    
+    cout << arr[b-1];
+}
