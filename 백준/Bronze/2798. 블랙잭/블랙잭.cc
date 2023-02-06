e4237c31ce1a3bdc88f7a8d021573d40a19fc7dc
Brilly-Bohyun <102973953+Brilly-Bohyun@users.noreply.github.com>
2023-02-06 11:25:58 +0900

    [Bronze II] Title: 블랙잭, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+// 브루트 포스 알고리즘
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n, m, result = 0;
+	int arr[101] = {};
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
+
+	for (int i = 0; i < n; i++) {
+		for (int j = i + 1; j < n; j++) {
+			for (int k = j + 1; k < n; k++) {
+				if (arr[i] + arr[j] + arr[k] <= m && arr[i] + arr[j] + arr[k] > result)
+					result = arr[i] + arr[j] + arr[k];
+			}
+		}
+	}
+	cout << result << '\n';
+}
