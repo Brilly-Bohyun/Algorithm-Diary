7d853dbd9fa7380af3389ba76afbd13d8745a648
Brilly-Bohyun <102973953+Brilly-Bohyun@users.noreply.github.com>
2023-05-15 09:42:39 +0900

    [unrated] Title: 주사위 게임 1, Time: 0.01 ms, Memory: 3.69 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int solution(int a, int b) {
+    int answer = 0;
+    
+    if(a % 2 == 0 && b % 2 ==0){
+        answer = abs(a - b);
+    } else if (a % 2 != 0 && b % 2 != 0){
+        answer = pow(a, 2) + pow(b, 2);
+    } else {
+        answer = 2 * (a + b);
+    }
+    
+    return answer;
+}
